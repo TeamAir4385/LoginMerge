151f186fd389c9a63bd802060e6ec9698ba83ebf
Lauren Alvarez <laurenaa010@gmail.com>
2015-04-23 14:50:11 -0500

    Added pushNotification.js and init. to index.js
    Added pushNotifications.js and initialization to index.js

--- Resources/android/alloy/controllers/index.js
+++ Resources/android/alloy/controllers/index.js
@@ -8,6 +8,32 @@ function __processArg(obj, key) {
 }
 
 function Controller() {
+    function initializePushNotifications(_user) {
+        Alloy.Globals.pushToken = null;
+        var pushLib = require("pushNotifications");
+        pushLib.initialize(_user, function(_pushData) {
+            Ti.API.info("I GOT A PUSH NOTIFICATION");
+            var payload;
+            try {
+                payload = _pushData.payload ? JSON.parse(_pushData.payload) : _pushData;
+            } catch (e) {
+                payload = {};
+            }
+            Ti.UI.createAlertDialog({
+                title: payload.android.title || "Alert",
+                message: payload.android.alert || "",
+                buttonNames: [ "Ok" ]
+            }).show();
+        }, function(_pushInitData) {
+            if (_pushInitData.success) {
+                Alloy.Globals.pushToken = _pushInitData.data.deviceToken;
+                Ti.API.debug("Success: Initializing Push Notifications " + JSON.stringify(_pushInitData));
+            } else {
+                alert("Error Initializing Push Notifications");
+                Alloy.Globals.pushToken = null;
+            }
+        });
+    }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "index";
     if (arguments[0]) {
@@ -30,6 +56,19 @@ function Controller() {
     exports.destroy = function() {};
     _.extend($, $.__views);
     $.home.getView().open();
+    $.loginSuccessAction = function(_options) {
+        initializePushNotifications(_options.model);
+        Ti.API.info("logged in user information");
+        Ti.API.info(JSON.stringify(_options.model, null, 2));
+        $.tabGroup.open();
+        $.tabGroup.setActiveTab(0);
+        $.feedController.initialize();
+        Alloy.Globals.currentUser = _options.model;
+        $.feedController.parentController = $;
+        $.friendsController.parentController = $;
+        $.settingsController.parentController = $;
+        $.loginController && $.loginController.close();
+    };
     _.extend($, exports);
 }
 
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -14,4 +14,91 @@ if (OS_ANDROID) {
 	$.index.open();
 }
 
+function initializePushNotifications(_user) {
+
+  Alloy.Globals.pushToken = null;
+  var pushLib = require('pushNotifications');
+
+  // initialize PushNotifications
+  pushLib.initialize(_user,
+  // notification received callback
+  function(_pushData) {
+    Ti.API.info('I GOT A PUSH NOTIFICATION');
+    // get the payload from the proper place depending
+    // on what platform you are on
+    var payload;
+
+    try {
+      if (_pushData.payload) {
+        payload = JSON.parse(_pushData.payload);
+      } else {
+        payload = _pushData;
+      }
+    } catch(e) {
+      payload = {};
+    }
+
+    // display the information in an alert
+    if (OS_ANDROID) {
+      Ti.UI.createAlertDialog({
+        title : payload.android.title || "Alert",
+        message : payload.android.alert || "",
+        buttonNames : ['Ok']
+      }).show();
+    } else {
+      Ti.UI.createAlertDialog({
+        title : "Alert",
+        message : payload.alert || "",
+        buttonNames : ['Ok']
+      }).show();
+    }
+
+  },
+  // registration callback parameter
+  function(_pushInitData) {
+    if (_pushInitData.success) {
+      // save the token so we know it was initialized
+      Alloy.Globals.pushToken = _pushInitData.data.deviceToken;
+
+      Ti.API.debug("Success: Initializing Push Notifications " + JSON.stringify(_pushInitData));
+    } else {
+      alert("Error Initializing Push Notifications");
+      Alloy.Globals.pushToken = null;
+    }
+  });
+}
+
+
+
+
+$.loginSuccessAction = function(_options) {
+
+  initializePushNotifications(_options.model);
+
+  Ti.API.info('logged in user information');
+  Ti.API.info(JSON.stringify(_options.model, null, 2));
+
+  // open the main screen
+  $.tabGroup.open();
+
+  // set tabGroup to initial tab, incase this is coming from
+  // a previously logged in state
+  $.tabGroup.setActiveTab(0);
+
+  // pre-populate the feed with recent photos
+  $.feedController.initialize();
+
+  // get the current user
+  Alloy.Globals.currentUser = _options.model;
+
+  // set the parent controller for all of the tabs, give us
+  // access to the global tab group and misc functionality
+  $.feedController.parentController = $;
+  $.friendsController.parentController = $;
+  $.settingsController.parentController = $;
+
+  // do any necessary cleanup in login controller
+  $.loginController && $.loginController.close();
+};
+
 //code runs on Android without this. 
